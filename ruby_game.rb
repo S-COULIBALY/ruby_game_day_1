f8dfadb0e0c23c6ae2b64a9de2a1caf7178ebf0a
S-COULIBALY <s.coulibaly@outlook.com>
2017-06-06 14:42:47 +0200

    Utilisation du hash pour le player

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,12 +1,11 @@
 nb_cols = 5
 nb_rows = 4
 
-player_x = 2
-player_y = 3
+player = { "x" => 2 , "y" => 3}
 
 for n in 1..nb_rows
   for m in 1..nb_cols
-    if n == player_x && m == player_y
+    if n == player["x"] && m == player["y"]
       print "P"
     else
       print "."
