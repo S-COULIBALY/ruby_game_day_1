d8fcca846858a7ce23dd4a19e23603c40ab8c2db
S-COULIBALY <s.coulibaly@outlook.com>
2017-06-06 14:34:52 +0200

    Positionnement du joueue 'p'

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,9 +1,16 @@
 nb_cols = 5
 nb_rows = 4
 
+player_x = 2
+player_y = 3
+
 for n in 1..nb_rows
   for m in 1..nb_cols
-    print "X"
+    if n == player_x && m == player_y
+      print "P"
+    else
+      print "."
+    end
   end
   puts
 end
