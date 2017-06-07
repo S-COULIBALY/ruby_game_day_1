2742c88ef8cd0b71ce28e4f5a89b7f4e38c5d19f
S-COULIBALY <s.coulibaly@outlook.com>
2017-06-07 12:39:18 +0200

    Creation de la class 'Ruby' et mise à jour du fichier ruby_game.rb

---
+++ ruby.rb
@@ -0,0 +1,3 @@
+class Ruby < StaticObject
+
+end
--- ruby_game.rb
+++ ruby_game.rb
@@ -6,15 +6,19 @@
 # require_relative('player.rb')
 require_relative 'static_objet'
 require_relative 'player'
+require_relative 'ruby'
 
 
 paul = Player.new(3, 4)
+ruby = Ruby.new(1, 1)
 
-def draw(nb_cols, nb_rows, player)
+def draw(nb_cols, nb_rows, player , ruby)
   for n in 1..nb_rows
     for m in 1..nb_cols
       if n == player.x && m == player.y
         print "P"
+      elsif n == ruby.x && m == ruby.y
+        print "R"
       else
         print "."
       end
@@ -25,7 +29,7 @@ def draw(nb_cols, nb_rows, player)
 end
 # draw(10, 15, { x: 9 , y: 3})
 # draw(10, 15, x: 9 , y: 3)
-draw 10, 15, paul
+draw 10, 15, paul, ruby
 puts
 puts
 
@@ -37,4 +41,4 @@ puts
 puts paul.inspect
 paul.move(11, 5)
 puts paul.inspect
-draw 10, 15, paul
+draw 10, 15, paul, ruby
